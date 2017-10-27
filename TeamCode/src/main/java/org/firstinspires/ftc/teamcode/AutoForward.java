7faa6618a0a2e05821d73a5ed18c70692005ef81
myriath <mitchhudson2002@gmail.com>
2017-10-26 20:48:38 -0500

    Added AutoForward class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoForward.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@Autonomous(name = "AutoForward", group = "Pushbot")
+
+public class AutoForward extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
