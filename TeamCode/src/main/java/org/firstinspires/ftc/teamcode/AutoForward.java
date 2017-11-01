9a3424905deddabef846b39082b0e06ce6f3fb7a
myriath <mitchhudson2002@gmail.com>
2017-10-31 20:33:19 -0500

    Added temporary duration of autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoForward.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoForward.java
@@ -15,9 +15,9 @@ public class AutoForward extends OpMode {
 
 
     private double Speed = 0.4;
-    private double secPerFt = 0.5 / Speed;
-    private double straightDist = 3;
-    private double straightDur = straightDist * secPerFt;
+    //    private double secPerFt = 0.5 / Speed;
+//    private double straightDist = 3;
+    private double straightDur = 0.5; //straightDist * secPerFt;
 
     private ElapsedTime timer = new ElapsedTime();
 
