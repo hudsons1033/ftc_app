824562c181beb8ad414a718b60754a7d9d468e50
myriath <mitchhudson2002@gmail.com>
2018-08-30 19:23:35 -0500

    modulated code start

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorBreakout.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+class MotorBreakout {
+
+    private DcMotor motor;
+
+    public MotorBreakout(DcMotor motor) {
+        this.motor = motor;
+    }
+
+    public void setDirection(DcMotor.Direction direction) {
+        motor.setDirection(direction);
+    }
+
+    public void setPower(double power) {
+        motor.setPower(power);
+    }
+}
