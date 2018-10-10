ae5e608467694e636e452321fdeeab3fdb4f852b
myriath <31975310+myriath@users.noreply.github.com>
2018-10-09 19:25:55 -0500

    Fixed multiple teleop error

--- TeamCode/src/main/java/ExampleCode/AutoTest.java
+++ TeamCode/src/main/java/ExampleCode/AutoTest.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-@Autonomous(name = "AutoTest", group = "Pushbot")
+//@Autonomous(name = "AutoTest", group = "Pushbot")
 
 public class AutoTest extends OpMode {
 
