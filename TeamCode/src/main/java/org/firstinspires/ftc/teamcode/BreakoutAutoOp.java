e6ea66e5518945bb1fa516b2712aad1b39459af4
myriath <31975310+myriath@users.noreply.github.com>
2018-12-01 11:34:25 -0600

    added tensorflow support

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BreakoutAutoOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BreakoutAutoOp.java
@@ -3,16 +3,23 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.vuforia.Vuforia;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
 
 import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection.BACK;
 import static org.firstinspires.ftc.teamcode.BreakoutMotor.Direction.MOTOR_R;
@@ -27,16 +34,24 @@ import static org.firstinspires.ftc.teamcode.BreakoutServo.Direction.SERVO_R;
 
 public class BreakoutAutoOp extends OpMode {
 
+    private int goldLoc;
+    private static final String TFOD_MODEL_ASSET = "RoverRuckus.tflite";
+    private static final String LABEL_GOLD_MINERAL = "Gold Mineral";
+    private static final String LABEL_SILVER_MINERAL = "Silver Mineral";
     private static final String VUFORIA_KEY = "AT5/naD/////AAABmaUvi2GyUUirkbnkPyo71cQmkhAaUVAKxkF96tVK5PueT2cxswHD7dT7ZBvwEBS4oPEDsajO7QdUFF0VtshBOnfi5AFTlL5pfgmMQhr2ceo9xTa+pyw71Bvym3qvmv5utemN+yU5b8eQfejOyfCh/EGpSvclNB9JAa5125hevdhFoYOT7BU6/Xx1wBAith2m/RPesA1tqqBfjWC4KfBonwCtLa0+erPLNnGt0PJiM0CIxyFTo4u9V4AP1TQm/VooDJ8+yXN7ZiIimPG42+0ahoYOdBK6Tc3jX2idV4YCSPBv8Gz6Ut9fu9VLMiYkDUeGZ9prKeehGh7NtttajEe6Dw/h3ww1sCoKOTfJvK0dWi3h";
-
+    private int captureCounter = 0;
+    private File captureDirectory = AppUtil.ROBOT_DATA_DIR;
+    private TFObjectDetector tfod;
     private static final VuforiaLocalizer.CameraDirection CAMERA_CHOICE = BACK;
-
+    private WebcamName webcamName;
     private boolean found = false;
     private boolean firstRun = true;
+    private boolean run = true;
     private String trackableName = "";
     double i = 0;
 
     private List<VuforiaTrackable> allTrackables = new ArrayList<>();
+    private List<VuforiaTrackable> allTrackables2 = new ArrayList<>();
     private List<BreakoutMotor> allMotors = new ArrayList<>();
     private List<BreakoutServo> allServos = new ArrayList<>();
 
@@ -119,56 +134,158 @@ public class BreakoutAutoOp extends OpMode {
 //        telemetry.update();
 //    }
 
-    private void blueRover() {
-        while(startTime.milliseconds() < 1700) {
+    private void blueRover(int goldLoc) {
+        while (startTime.milliseconds() < 1700) {
             motorLeft.setPower(1);
             motorRight.setPower(1);
         }
         motorLeft.setPower(0);
         motorRight.setPower(0);
-        while(startTime.milliseconds() < 2000) {
+        motorSweeperArm.setPower(0);
+        motorSweeper.setPower(0);
+        while (startTime.milliseconds() < 2000) {
             motorSweeperArm.setPower(1);
         }
+        motorLeft.setPower(0);
+        motorRight.setPower(0);
         motorSweeperArm.setPower(0);
-        while(startTime.milliseconds() < 2500) {
+        motorSweeper.setPower(0);
+        while (startTime.milliseconds() < 2500) {
             motorSweeper.setPower(-1);
         }
+        motorLeft.setPower(0);
+        motorRight.setPower(0);
+        motorSweeperArm.setPower(0);
         motorSweeper.setPower(0);
-
-    }
-
-    private void redFootprint() {
-        while(startTime.milliseconds() < 1700) {
-            motorLeft.setPower(1);
-            motorRight.setPower(1);
-        }
-        while(startTime.now(TimeUnit.MILLISECONDS) < 2700) {
-            motorLeft.setPower(0);
-            motorRight.setPower(0);
-            motorSweeperArm.setPower(1);
-            motorSweeper.setPower(1);
-        }
         stop();
     }
 
-    private void frontCraters() {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    private void redFootprint(int goldLoc) {
+        while (startTime.milliseconds() < 1700) {
+            motorLeft.setPower(1);
+            motorRight.setPower(1);
         }
+        motorLeft.setPower(0);
+        motorRight.setPower(0);
+        motorSweeperArm.setPower(0);
+        motorSweeper.setPower(0);
+        while (startTime.milliseconds() < 2000) {
+            motorSweeperArm.setPower(1);
+        }
+        motorLeft.setPower(0);
+        motorRight.setPower(0);
+        motorSweeperArm.setPower(0);
+        motorSweeper.setPower(0);
+        while (startTime.milliseconds() < 2500) {
+            motorSweeper.setPower(-1);
+        }
+        motorLeft.setPower(0);
+        motorRight.setPower(0);
+        motorSweeperArm.setPower(0);
+        motorSweeper.setPower(0);
+        stop();
     }
 
-    private void backSpace() {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    private void frontCraters(int goldLoc) {
+        while (startTime.milliseconds() < 1700) {
+            motorLeft.setPower(1);
+            motorRight.setPower(1);
         }
+        motorLeft.setPower(0);
+        motorRight.setPower(0);
+        motorSweeperArm.setPower(0);
+        motorSweeper.setPower(0);
+        while (startTime.milliseconds() < 2000) {
+            motorSweeperArm.setPower(1);
+        }
+        motorLeft.setPower(0);
+        motorRight.setPower(0);
+        motorSweeperArm.setPower(0);
+        motorSweeper.setPower(0);
+        while (startTime.milliseconds() < 2500) {
+            motorSweeper.setPower(-1);
+        }
+        motorLeft.setPower(0);
+        motorRight.setPower(0);
+        motorSweeperArm.setPower(0);
+        motorSweeper.setPower(0);
+        stop();
+    }
+
+    private void backSpace(int goldLoc) {
+        while (startTime.milliseconds() < 1700) {
+            motorLeft.setPower(1);
+            motorRight.setPower(1);
+        }
+        motorLeft.setPower(0);
+        motorRight.setPower(0);
+        motorSweeperArm.setPower(0);
+        motorSweeper.setPower(0);
+        while (startTime.milliseconds() < 2000) {
+            motorSweeperArm.setPower(1);
+        }
+        motorLeft.setPower(0);
+        motorRight.setPower(0);
+        motorSweeperArm.setPower(0);
+        motorSweeper.setPower(0);
+        while (startTime.milliseconds() < 2500) {
+            motorSweeper.setPower(-1);
+        }
+        motorLeft.setPower(0);
+        motorRight.setPower(0);
+        motorSweeperArm.setPower(0);
+        motorSweeper.setPower(0);
+        stop();
+    }
+
+    private void sweeperOut() {
+        motorHorizontal.setPower(1);
+        motorHorizontalBack.setPower(1);
+    }
+
+    private void sweeperIn() {
+        motorHorizontal.setPower(-1);
+        motorHorizontalBack.setPower(-1);
+    }
+
+    private void initTfod(VuforiaLocalizer vuforia) {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_GOLD_MINERAL, LABEL_SILVER_MINERAL);
     }
 
     @Override
     public void init() {
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+        VuforiaLocalizer.Parameters parameters2 = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraDirection = CAMERA_CHOICE;
+        parameters2.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters2.cameraName = webcamName;
+
+        AppUtil.getInstance().ensureDirectoryExists(captureDirectory);
+        VuforiaLocalizer vuforia = ClassFactory.getInstance().createVuforia(parameters);
+        VuforiaLocalizer vuforiaTensor = ClassFactory.getInstance().createVuforia(parameters2);
+        vuforiaTensor.enableConvertFrameToBitmap();
+        VuforiaTrackables targetsRoverRuckus = vuforia.loadTrackablesFromAsset("RoverRuckus");
+        VuforiaTrackable blueRover = targetsRoverRuckus.get(0);
+        blueRover.setName("Blue-Rover");
+        VuforiaTrackable redFootprint = targetsRoverRuckus.get(1);
+        redFootprint.setName("Red-Footprint");
+        VuforiaTrackable frontCraters = targetsRoverRuckus.get(2);
+        frontCraters.setName("Front-Craters");
+        VuforiaTrackable backSpace = targetsRoverRuckus.get(3);
+        backSpace.setName("Back-Space");
+
+        allTrackables.addAll(targetsRoverRuckus);
+
+        targetsRoverRuckus.activate();
+        startTime.reset();
 
         //Broken out motorSweeper class
         motorLeft.set(hardwareMap.dcMotor.get("motorLeft"));
@@ -181,10 +298,17 @@ public class BreakoutAutoOp extends OpMode {
         motorRight.setDirection(MOTOR_R);
         motorSweeper.setDirection(MOTOR_F);
         motorSweeperArm.setDirection(MOTOR_F);
-        motorHorizontal.setDirection(MOTOR_F); //?
-        motorHorizontalBack.setDirection(MOTOR_R); //?
-        allMotors.add(motorLeft); allMotors.add(motorRight); allMotors.add(motorSweeper); allMotors.add(motorSweeperArm); allMotors.add(motorHorizontal); allMotors.add(motorHorizontalBack);
+        motorHorizontal.setDirection(MOTOR_F);
+        motorHorizontalBack.setDirection(MOTOR_R);
+        allMotors.add(motorLeft);
+        allMotors.add(motorRight);
+        allMotors.add(motorSweeper);
+        allMotors.add(motorSweeperArm);
+        allMotors.add(motorHorizontal);
+        allMotors.add(motorHorizontalBack);
         allSet(allMotors, 1);
+
+        webcamName = hardwareMap.get(WebcamName.class, "Webcam");
 
         //Broken out Gyro class
         gyroA.set(hardwareMap.get(gyroA.IMU, "gyroA"));
@@ -193,6 +317,16 @@ public class BreakoutAutoOp extends OpMode {
         gyroA.calibrate();
         telemetry.clearAll();
         telemetry.update();
+
+        if (ClassFactory.getInstance().canCreateTFObjectDetector()) {
+            initTfod(vuforiaTensor);
+        } else {
+            telemetry.addData("Sorry!", "This device is not compatible with TFOD");
+        }
+
+        if (tfod != null) {
+            tfod.activate();
+        }
     }
 
     @Override
@@ -204,37 +338,15 @@ public class BreakoutAutoOp extends OpMode {
     @Override
     public void loop() {
 
-        if (firstRun) {
-            firstRun = false;
-
-            int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-            VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
-
-            parameters.vuforiaLicenseKey = VUFORIA_KEY;
-            parameters.cameraDirection = CAMERA_CHOICE;
-
-            VuforiaLocalizer vuforia = ClassFactory.getInstance().createVuforia(parameters);
-            VuforiaTrackables targetsRoverRuckus = vuforia.loadTrackablesFromAsset("RoverRuckus");
-            VuforiaTrackable blueRover = targetsRoverRuckus.get(0);
-            blueRover.setName("Blue-Rover");
-            VuforiaTrackable redFootprint = targetsRoverRuckus.get(1);
-            redFootprint.setName("Red-Footprint");
-            VuforiaTrackable frontCraters = targetsRoverRuckus.get(2);
-            frontCraters.setName("Front-Craters");
-            VuforiaTrackable backSpace = targetsRoverRuckus.get(3);
-            backSpace.setName("Back-Space");
-
-            allTrackables.addAll(targetsRoverRuckus);
-
-            targetsRoverRuckus.activate();
-            startTime.reset();
-        }
-
         if (!found) {
-            motorLeft.setPower(1);
-            motorRight.setPower(-1);
+            while (startTime.milliseconds() < 100) {
+                motorLeft.setPower(1);
+                motorRight.setPower(1);
+            }
+            motorLeft.setPower(-0.2);
+            motorRight.setPower(0.2);
             for (VuforiaTrackable trackable : allTrackables) {
-                if (((VuforiaTrackableDefaultListener)trackable.getListener()).isVisible()) {
+                if (((VuforiaTrackableDefaultListener) trackable.getListener()).isVisible()) {
                     this.trackableName = trackable.getName();
                     telemetry.addData("Visible Target", trackable.getName());
                     found = true;
@@ -243,25 +355,68 @@ public class BreakoutAutoOp extends OpMode {
                 }
             }
         }
-
-        switch (trackableName) {
-            case "Blue-Rover":
-                blueRover();
-                break;
-            case "Red-Footprint":
-                redFootprint();
-                break;
-            case "Front-Craters":
-                frontCraters();
-                break;
-            case "Back-Space":
-                backSpace();
-                break;
-            case "":
-                startTime.reset();
-                break;
+        if (tfod != null) {
+            // getUpdatedRecognitions() will return null if no new information is available since
+            // the last time that call was made.
+            List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+            if (updatedRecognitions != null) {
+                telemetry.addData("# Object Detected", updatedRecognitions.size());
+                if (updatedRecognitions.size() == 3) {
+                    int goldMineralX = -1;
+                    int silverMineral1X = -1;
+                    int silverMineral2X = -1;
+                    for (Recognition recognition : updatedRecognitions) {
+                        if (recognition.getLabel().equals(LABEL_GOLD_MINERAL)) {
+                            goldMineralX = (int) recognition.getLeft();
+                        } else if (silverMineral1X == -1) {
+                            silverMineral1X = (int) recognition.getLeft();
+                        } else {
+                            silverMineral2X = (int) recognition.getLeft();
+                        }
+                    }
+                    if (goldMineralX != -1 && silverMineral1X != -1 && silverMineral2X != -1) {
+                        if (goldMineralX < silverMineral1X && goldMineralX < silverMineral2X) {
+                            telemetry.addData("Gold Mineral Position", "Left");
+                            goldLoc = 0;
+                        } else if (goldMineralX > silverMineral1X && goldMineralX > silverMineral2X) {
+                            telemetry.addData("Gold Mineral Position", "Right");
+                            goldLoc = 1;
+                        } else {
+                            telemetry.addData("Gold Mineral Position", "Center");
+                            goldLoc = 2;
+                        }
+                    }
+                }
+                telemetry.update();
+            }
         }
 
+        if(run) {
+            switch (trackableName) {
+                case "Blue-Rover":
+                    startTime.reset();
+                    blueRover(goldLoc);
+                    run = false;
+                    break;
+                case "Red-Footprint":
+                    startTime.reset();
+                    redFootprint(goldLoc);
+                    run = false;
+                    break;
+                case "Front-Craters":
+                    startTime.reset();
+                    frontCraters(goldLoc);
+                    run = false;
+                    break;
+                case "Back-Space":
+                    startTime.reset();
+                    backSpace(goldLoc);
+                    run = false;
+                    break;
+                case "":
+                    break;
+            }
+        }
     }
 
     @Override
